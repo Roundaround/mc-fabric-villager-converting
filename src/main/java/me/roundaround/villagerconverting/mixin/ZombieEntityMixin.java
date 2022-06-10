3df4ffcaa6f064403aec788e2b8e11f932b2cb0a
Evan Steinkerchner <esteinkerchner@gmail.com>
2022-06-10 15:36:00 -0400

    Make mixin target more generic

--- src/main/java/me/roundaround/villagerconverting/mixin/ZombieEntityMixin.java
+++ src/main/java/me/roundaround/villagerconverting/mixin/ZombieEntityMixin.java
@@ -15,7 +15,7 @@ import net.minecraft.world.WorldEvents;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import me.roundaround.villagerconverting.VillagerConvertingMod;
 
@@ -25,8 +25,8 @@ public abstract class ZombieEntityMixin extends HostileEntity {
     super(entityType, world);
   }
 
-  @Inject(method = "onKilledOther(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/LivingEntity;)V", at = @At("HEAD"), cancellable = true)
-  private void onKilledOther(ServerWorld world, LivingEntity other, CallbackInfo callbackInfo) {
+  @Inject(method = "onKilledOther", at = @At("HEAD"), cancellable = true)
+  private void onKilledOther(ServerWorld world, LivingEntity other, CallbackInfoReturnable<Boolean> callbackInfo) {
     if (!VillagerConvertingMod.CONFIG.MOD_ENABLED.getValue()) {
       // Mod is disabled, fall back to vanilla behavior.
       return;
