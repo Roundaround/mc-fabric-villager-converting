bc0ea4f91812d95843792dd43201ac66bdb396c6
Evan Steinkerchner <esteinkerchner@gmail.com>
2023-02-08 14:57:59 -0500

    Update to 1.19.3

--- src/main/java/me/roundaround/villagerconverting/config/modmenu/VillagerConvertingConfig.java
+++ src/main/java/me/roundaround/villagerconverting/config/modmenu/VillagerConvertingConfig.java
@@ -14,18 +14,20 @@ public class VillagerConvertingConfig extends ModConfig {
     MOD_ENABLED = registerConfigOption(
         BooleanConfigOption
             .builder("modEnabled", "villagerconverting.mod_enabled.label")
-            .setComment("Simple toggle for the mod! When set to false, the \n"
-                + " villagers will fall back to vanilla behavior/probability \n"
-                + " for zombie-conversion.")
+            .setComment(
+                "Simple toggle for the mod! When set to false, the",
+                "villagers will fall back to vanilla behavior/probability",
+                "for zombie-conversion.")
             .build());
     REQUIRE_NAME = registerConfigOption(
         BooleanConfigOption
             .yesNoBuilder("requireName", "villagerconverting.require_name.label")
             .setDefaultValue(false)
-            .setComment("When set to true, only villagers that have a custom \n"
-                + " name set (i.e. with a nametag) will be guaranteed to \n"
-                + " convert! Non-named villagers will fall back to vanilla \n"
-                + " behavior.")
+            .setComment(
+                "When set to true, only villagers that have a custom",
+                "name set (i.e. with a nametag) will be guaranteed to",
+                "convert! Non-named villagers will fall back to vanilla",
+                "behavior.")
             .build());
   }
 }
